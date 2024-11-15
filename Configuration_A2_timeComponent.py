3f04fceef35b30a139083aa391daad9cead2b27e
Ron <rm_ebner@mailbox.org>
2024-11-15 11:20:54 +0100

    added time component to all configurations
    added time component to all three configurations. running mini test

--- Configuration_A2_timeComponent.py
+++ Configuration_A2_timeComponent.py
@@ -13,23 +13,24 @@ A question of time and space:
 Ebner&Meijer, 2024
 _______________________________________________________________________________
 
-Configuration A1
+Configuration A2
 
-This script calculates the salinties for the different boxes for the A1 
+This script calculates the salinties for the different boxes for the A2 
 configuration for multiple values of e,g,f,c.
-
 
 It's output describes the timespan it takes for the marginal box to reach 
 halite saturation after the open box has reached gypsum
+
 """
 
 import           numpy   as np
 
 #%% time and stuff
-print( "initiate" )
 yr2sc = 24*60*60*365.25
-dt    = 0.5
 
+dt = 0.5
+DT =dt*yr2sc
+t_max = 40_000
 
 #%% 
 
@@ -47,59 +48,70 @@ G   =np.logspace(2, 7, num=15)
 E_step = 12.5
 Etmp   = np.arange(12.5, 80, E_step)
 E      = Etmp/(100*yr2sc)
-#scenario A1 (convection)
-Ctmp   = np.array([10,50,90])
-C      = Ctmp/(100)
+#
+#scenario A2 (restricted margin)
+C      = np.array([10**2, 10**4, 10**6])
 
 kappa_mix   = 1*10**(-4)
 kappa_conv  = 1*10**(-1)
 d_mix       = 0.5*D0
 
-arrSA1 = np.zeros((3, len(G), len(C), len(E), len(F)))
-arrFA1 = np.zeros((2, len(G), len(C), len(E), len(F)))
+
+
+
+S    = np.ones((3, int(t_max/dt)))*SA
+
+arrSA2 = np.zeros((3, len(G), len(C), len(E), len(F)))
+arrFA2 = np.zeros((2, len(G), len(C), len(E), len(F)))
 arrTime = np.zeros((2, len(G), len(C), len(E), len(F)))
+
 #%% loop
-print( "start loop" )
 fi=0
 for f in F:
     V    = np.array([f*A0*Dint,(1-f)*A0*Dint, A0*(D0-Dint) ])
     ic=0
-    for c in C:
+    for A2c in C:
         ie =0
         for e in E:
             ig = 0
             for g in  G:
-                # t-max increases with decreasing g to
-                # 1. make sure that equlibrium is reached (small g)
-                # 2. minimise the runtime (large g)
+                #%%
+                # Scenario A2
                 t_max = 1_000_000/(np.log10(g))**2
                 S    = np.ones((3, int(t_max/dt)+1))*SA
                 t=0
                 ii=0
                 while t< t_max-dt:
+                    # Scenario A2
                     t+=dt
-                    
+            
                     Q= g*np.sqrt(S[1,ii]-SA)
                     Qin= Q+ e*A0
+            
+                    F02 = (S[0,ii]>S[1,ii])*(S[0,ii]>S[2,ii])*A2c*(S[0,ii]  - S[2,ii])
+                    F01 = (S[0,ii]>S[1,ii])*(S[0,ii]<S[2,ii])*A2c*(S[0,ii]  - S[1,ii])
+                    F10 = F02 + F01 +e*A0*f
+                    F21 = F02
                     
-                    F02 = (S[0, ii]>S[2, ii])*kappa_conv*f*A0*(S[0,ii]-S[2,ii])/(S[2,ii]*d_mix)
-                    F20 = (1-c)*F02
-                    F21 = c*F02
-                    F10 = c*F02 + e*A0*f
                     mix = kappa_mix*A0*(1-f)*(S[2, ii]-S[1, ii])/d_mix
             
-                    S[0,ii+1]= min(SH, S[0,ii] + (F10*S[1,ii] + F20*S[2,ii] 
-                                          - F02*S[0,ii])*dt*yr2sc/V[0])
-                    S[1,ii+1]= min(SH, S[1,ii] + (Qin*SA + F21*S[2, ii] 
-                                          -(F10 + Q)*S[1, ii]  
-                                          + mix)*dt*yr2sc/V[1])
-                    S[2,ii+1]= min(SH, S[2,ii] + (F02*S[0,ii] 
-                                          - (F21 + F20)*S[2,ii] 
-                                          - mix)*dt*yr2sc/V[2])
+                    S[0, ii+1]= min(SH, S[0,ii] 
+                                        + (F10*S[1,ii] 
+                                           - (F01 +F02)*S[0,ii])
+                                                *DT/V[0])
+                    S[1, ii+1]= min(SH, S[1,ii] 
+                                        + (Qin*SA + F21*S[2,ii] + F01*S[0,ii] 
+                                           - (F10+Q)*S[1, ii] + mix)
+                                                *DT/V[1])
+                    S[2, ii+1]= min(SH, S[2,ii] 
+                                        + (F02*S[0,ii] 
+                                           - F21*S[2,ii] -mix)
+                                                *DT/V[2])
                     ii+=1
                 time_1_gypsum = 0
                 time_0_halite = 0
                 time_1_halite = 0
+                
                 if max(S[0,:])>= SH: # margin has reached halite
                     time_1_gypsum = np.searchsorted(S[1,:], SG)*dt
                     time_0_halite = np.searchsorted(S[0,:], SH)*dt
@@ -108,48 +120,45 @@ for f in F:
 
                             
                 DT_G1_H0 = time_0_halite - time_1_gypsum
-                DT_G1_H1 = time_1_halite - time_1_gypsum
-                arrTime[:, ig, ic, ie, fi] = [DT_G1_H0, DT_G1_H1]
-                arrSA1[:, ig, ic, ie, fi]= [S[0,ii], S[1,ii], S[2,ii]]
-                arrFA1[:, ig, ic, ie, fi]= [Q,F02]
+                DT_G1_H1 = time_1_halite - time_1_gypsum    
+                arrSA2[:, ig, ic, ie, fi]= [S[0,ii], S[1,ii], S[2,ii]]
+                arrFA2[:, ig, ic, ie, fi]= [Q,F02]
                 
                 ig+=1
             ie+=1
         ic+=1
     fi+=1
-
             
 #%% Save arrays in txt files
-print( "begin saving" )
-name_dir="DATA/Output_ScenA1_mini_time"
+name_dir="DATA/Output_ScenA2_mini_time"
 ci=0
 for c in C:
     fi=0
     for f in F:
         #  %5.4f' %(3.141592))
-        name_file = name_dir+"_f=" + "%03d"%((int(f*100))) + "_c=" + "%03d"%(int(c*100)) #%(int(np.log10(c)))) 
+        name_file = name_dir+"_f=" + "%03d"%((int(f*100))) + "_c=" + "%03d"%(int(np.log10(c))) #%(int(np.log10(c)))) 
         
         #S0
-        data = np.squeeze(arrSA1[0,:,ci, :, fi])
+        data = np.squeeze(arrSA2[0,:,ci, :, fi])
         np.savetxt(name_file+"_S0.txt",data,delimiter=",")
         
         #S1
-        data = np.squeeze(arrSA1[1,:,ci, :, fi])
+        data = np.squeeze(arrSA2[1,:,ci, :, fi])
         np.savetxt(name_file+"_S1.txt",data,delimiter=",")
         
         #S2
-        data = np.squeeze(arrSA1[2,:,ci, :, fi])
+        data = np.squeeze(arrSA2[2,:,ci, :, fi])
         np.savetxt(name_file+"_S2.txt",data,delimiter=",")
         
         #Q
-        data = np.squeeze(arrFA1[0,:,ci, :, fi])
+        data = np.squeeze(arrFA2[0,:,ci, :, fi])
         np.savetxt(name_file+"_Q.txt",data,delimiter=",")
         
         #Q
-        data = np.squeeze(arrFA1[1,:,ci, :, fi])
+        data = np.squeeze(arrFA2[1,:,ci, :, fi])
         np.savetxt(name_file+"_F.txt",data,delimiter=",")
         
-        #Time between gypsum and Halite
+                #Time between gypsum and Halite
         data = np.squeeze(arrTime[0,:,ci, :, fi])
         np.savetxt(name_file+"_Time_G1_H0.txt",data,delimiter=",")
         
@@ -166,4 +175,3 @@ np.savetxt(name_dir+"E.txt",data,delimiter=",")
 #G
 data = np.squeeze(G)
 np.savetxt(name_dir+"G.txt",data,delimiter=",")
-print( "saving successful" )
