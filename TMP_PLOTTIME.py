8003f4b78400996cc7fd6b27b403109d1edaaf7f
Ronja <rm_ebner@mailbox.org>
2025-01-14 20:41:21 +0100

    Update TMP_PLOTTIME.py
    changed legends

--- TMP_PLOTTIME.py
+++ TMP_PLOTTIME.py
@@ -120,10 +120,45 @@ legend_elements_2 = [Line2D([0], [0], color=colorA1, alpha = 0.5, lw=4, label='A
                      Line2D([0], [0], color=colorA1, alpha = 0.5, lw=4, linestyle= ':', label='A1 Halite'),
                      Line2D([0], [0], color=colorA2, alpha = 0.5, lw=4, linestyle= ':',  label='A2 Halite'),
                      Line2D([0], [0], color=colorB , alpha = 0.5, lw=4, linestyle= ':', label='B Halite')]
+
+legend_elements_3 = [Line2D([0], [0], color='k', alpha = 0.5, lw=3,linestyle= '-', label='Gypsum'),
+                     Line2D([0], [0], color='k', alpha = 0.5, lw=3,linestyle= ':', label='Halite'),
+                     Line2D([0], [0], color='w', alpha = 0.0, lw=1,linestyle= ':', label=' '),
+                     Line2D([0], [0], color='k' , alpha = 0.5, lw=2              , label='e =0.25 m/yr'),
+                     Line2D([0], [0], color='k' , alpha = 0.5, lw=3              , label='e =0.75 m/yr'),
+                     Line2D([0], [0], color='w', alpha = 0.0, lw=1,linestyle= ':', label=' '),
+                     Line2D([0], [0], color=colorA1, alpha = 0.5, lw=3, label='A1'),
+                     Line2D([0], [0], color=colorA2, alpha = 0.5, lw=3, label='A2'),
+                     Line2D([0], [0], color=colorB , alpha = 0.5, lw=3, label='B')]
+
+legend_elements_4 = [Line2D([0], [0], color='k' , alpha = 0.5, lw=2              , label='e =0.25 m/yr'),
+                     Line2D([0], [0], color='k' , alpha = 0.5, lw=3              , label='e =0.75 m/yr'),
+                     Line2D([0], [0], color='w', alpha = 0.0, lw=1,linestyle= ':', label=' '),
+                     Line2D([0], [0], color=colorA1, alpha = 0.5, lw=3, label='A1'),
+                     Line2D([0], [0], color=colorA2, alpha = 0.5, lw=3, label='A2'),
+                     Line2D([0], [0], color=colorB , alpha = 0.5, lw=3, label='B')]
+
 color_conf = [colorA1, colorA2, colorB]
 
 y_min = 0
 y_max = 130
+
+
+def get_last_g(conf, iie, iic, T_H, T_G, case):
+    if case == 'first':
+        y1 = T_G[ :, iic1, iie, iif1, conf]/1000
+    elif case == 'met':
+        y1 = ((T_All_H[ :, iic, iie, iif1, conf] - 
+               np.maximum(T_First_H[ :, iic, iie, iif1, conf], 
+                          T_All_G[ :, iic, iie, iif1, conf])   )/1000)
+    else:
+        print('Case must be either "first" or "met"')
+        
+    y1[y1==0.0]= np.nan
+    iig = np.where(np.isnan(y1))
+    return iig
+
+
 #%% PLot salinity for different configurations
 iif1=  0
 iif2= -1
@@ -405,17 +440,31 @@ fig, ax = plt.subplots(dpi = 300)
 S= 1
 for conf in [0,1,2]:
     for iie in [1, 5]:
+        if iie == 1:
+            lw = 1
+        elif iie ==5:
+            lw = 2
         y1 = T_First_G[ :, iic1, iie, iif1, conf] /1000
         y2 = T_First_G[ :, iic2, iie, iif2, conf] /1000
 
-        ax.plot(G, y1 , color = color_conf[conf], linestyle = '-')
-        ax.plot(G, y2 , color = color_conf[conf], linestyle = '-')
+        ax.plot(G, y1 , color = color_conf[conf], linestyle = '-', linewidth = lw)
+        ax.plot(G, y2 , color = color_conf[conf], linestyle = '-', linewidth = lw)
         
         y1 = T_First_H[ :, iic1, iie, iif1, conf] /1000
         y2 = T_First_H[ :, iic2, iie, iif2, conf] /1000
         
-        ax.plot(G, y1 , color = color_conf[conf], linestyle = ':')
-        ax.plot(G, y2 , color = color_conf[conf], linestyle = ':')
+        ax.plot(G, y1 , color = color_conf[conf], linestyle = ':', linewidth = lw)
+        ax.plot(G, y2 , color = color_conf[conf], linestyle = ':', linewidth = lw)
+
+
+conf = 1
+iie = 5
+iic = 3
+iig = get_last_g(conf, iie, iic, T_First_H, T_First_G, 'first')
+ax.vlines(G[iig[0][0]-1], y_min, y_max , color = 'gray', alpha = 0.5)
+# ax.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
+# axins.plot(G, y1 , color = color_conf[conf], alpha = 0.5)
+# axins.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
 
 ax.set_xscale('log')
 ax.set_xlim([G[0], G[-1]])
@@ -428,7 +477,7 @@ ax.set_xlabel('low $\longleftarrow$ strait efficiency  $\longrightarrow$ high', 
 plt.ylabel('time [$kyr$]', fontsize= fs)
 plt.title("time to first gypsum and first halite", fontsize= fs*1.2)
 plt.ylim([y_min, y_max])
-plt.legend(handles=legend_elements_2, loc='upper right')
+plt.legend(handles=legend_elements_3, loc='upper right')
 plt.tight_layout()
 fig.savefig( figname+ "time_first_both.png") 
 fig.savefig( figname+ "time_first_both.svg", format="svg") 
@@ -502,54 +551,48 @@ iic2=  -1
 fs  =  15
 
 fig, ax = plt.subplots(dpi = 300)
-axins = ax.inset_axes([0.35, 0.1, 0.45, 0.8])
+axins = ax.inset_axes([0.25, 0.1, 0.35, 0.8])
 S= 1
 for conf in [0,1,2]:
     for iie in [1, 5]:
         for iic in [0, -1]:
+            if iie == 1:
+                lw = 1
+            elif iie ==5:
+                lw = 2
             y1 = ((T_All_H[ :, iic, iie, iif1, conf] - 
                    np.maximum(T_First_H[ :, iic, iie, iif1, conf], 
                               T_All_G[ :, iic, iie, iif1, conf])   )/1000)
-            y2 = ((T_All_H[ :, iic2, iie, iif2, conf] - 
-                   np.maximum(T_First_H[ :, iic, iie, iif2, conf], 
-                              T_All_G[ :, iic2, iie, iif2, conf])   )/1000)
-            y1[y1==0.0]= np.nan
-            y2[y2==0.0]= np.nan
             
-            ax.plot(G, y1 , color = color_conf[conf])
-            ax.plot(G, y2 , color = color_conf[conf])
-            axins.plot(G, y1 , color = color_conf[conf])
-            axins.plot(G, y2 , color = color_conf[conf])
+            # y2 = ((T_All_H[ :, iic2, iie, iif2, conf] - 
+            #        np.maximum(T_First_H[ :, iic, iie, iif2, conf], 
+            #                   T_All_G[ :, iic2, iie, iif2, conf])   )/1000)
+            y1[y1==0.0]= np.nan
+            # y2[y2==0.0]= np.nan
+            
+            ax.plot(G, y1 , color = color_conf[conf], linewidth = lw)
+            # ax.plot(G, y2 , color = color_conf[conf], linewidth = lw)
+            axins.plot(G, y1 , color = color_conf[conf], linewidth = lw)
+            # axins.plot(G, y2 , color = color_conf[conf], linewidth = lw)
             
 # add asymptote
-
-def get_asymptote(conf, iie, iic, T_All_H, T_All_G):
-        
-    y1 = ((T_All_H[ :, iic, iie, iif1, conf] - 
-           np.maximum(T_First_H[ :, iic, iie, iif1, conf], 
-                      T_All_G[ :, iic, iie, iif1, conf])   )/1000)
-    y2 = ((T_All_H[ :, iic2, iie, iif2, conf] - 
-           np.maximum(T_First_H[ :, iic, iie, iif2, conf], 
-                      T_All_G[ :, iic2, iie, iif2, conf])   )/1000)
-    y1[y1==0.0]= np.nan
-    y2[y2==0.0]= np.nan
-    iig = np.where(np.isnan(y1))
-    return iig
 
 conf = 0
 iie = -1
 iic = 1
-iig = get_asymptote(conf, iie, iic, T_All_H, T_All_G)
-ax.vlines(G[iig[0][0]-1], y_min, y_max , color = color_conf[conf], alpha = 0.5)
-ax.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
+iig = get_last_g(conf, iie, iic, T_All_H, T_All_G, 'met')
+ax.vlines(G[iig[0][0]-1], y_min, y_max , color = 'gray', alpha = 0.5)
+axins.vlines(G[iig[0][0]-1], y_min, y_max , color = 'gray', alpha = 0.5)
+# ax.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
 axins.plot(G, y1 , color = color_conf[conf], alpha = 0.5)
-axins.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
+# axins.plot(G, y2 , color = color_conf[conf], alpha = 0.5)
             
 
 ax.set_xscale('log')
 ax.set_xlim([G[0], G[-1]])
 
-x1, x2, y1, y2 =G[0], 0.4*10**3, -1, 50
+x1, x2, y1, y2 =G[0], 0.4*10**3, 10**(-2), 50
+axins.set_yscale('log')
 axins.set_xlim(x1, x2)
 axins.set_ylim(y1, y2)
 ax.indicate_inset_zoom(axins, edgecolor="black")
@@ -566,7 +609,7 @@ ax.set_xlabel('low $\longleftarrow$ strait efficiency  $\longrightarrow$ high', 
 plt.ylabel('time [$kyr$]', fontsize= fs)
 plt.title("duration of simultaneous precipitation", fontsize= fs*1.2)
 
-plt.legend(handles=legend_elements, loc='upper right')
+plt.legend(handles=legend_elements_4, loc='upper right')
 plt.ylim([y_min, y_max])
 plt.tight_layout()
 fig.savefig( figname+ "duration_conditions_met_extra.png") 
